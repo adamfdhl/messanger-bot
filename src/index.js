25fc06310ed02ad2d149251b1964023791061b8f
Adam <ramadhana@garena.co.id>
2020-06-14 20:07:26 +0700

    Enabling func

--- src/index.js
+++ src/index.js
@@ -21,6 +21,41 @@ app.get("/webhook/", (req, res) => {
 	res.send("Wrong token");
 });
 
+app.post("/webhook/", (req, res) => {
+	let messaging_events = req.body.entry[0].messaging_events;
+	for (let i = 0; i < messaging_events.length; i++) {
+		let event = messaging_events[i];
+		let sender = event.sender.id;
+		if (event.message && event.message.text) {
+			let text = event.message.text;
+			sendText(sender, "Text echo: " + text.substring(0, 100));
+		}
+	}
+	res.sendStatus(200);
+});
+
+const sendText = (sender, text) => {
+	let messageData = { text: text };
+	request(
+		{
+			url: "https://graph.facebook.com/v7.0/me/message",
+			qs: { access_token: process.env.ACCESS_TOKEN },
+			method: "POST",
+			json: {
+				receipt: { id: sender },
+				message: messageData,
+			},
+		},
+		(error, response) => {
+			if (error) {
+				console.log("sending error: ", error);
+			} else if (response.body.error) {
+				console.log("response body error: ", response.body.error);
+			}
+		}
+	);
+};
+
 app.listen(app.get("port"), () => {
 	console.log(`Server running`);
 });
